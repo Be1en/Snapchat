eee1f5156dfb631414f6ccab7d2158b3b22ad36e
Be1en <valeria.godoy@tecsup.edu.pe>
2024-05-20 18:08:05 -0500

    cambios realizados con conexión a Firebase

---
+++ GodoySnapchat/iniciarSesionViewController.swift
@@ -0,0 +1,44 @@
+//
+//  iniciarSesionViewController.swift
+//  GodoySnapchat
+//
+//  Created by Belen Godoy on 20/05/24.
+//
+
+import UIKit
+import FirebaseAuth
+
+class iniciarSesionViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print("Intentando Iniciar Sesion")
+            if error != nil{
+                print ("Se presento el siguient error: \(error)")
+            }else{
+                print ("Inicio de sesion exitoso")
+            }
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
